a33f7aa925a840ffebae0a9002f7ca2517cf57b2
sagar608 <sagarkarki076@gmail.com>
2020-06-21 18:28:01 +0545

    online db

--- config/db_config.js
+++ config/db_config.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const dotenv = require("dotenv").config()
 
 
 const connectionOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
-const DB_URI = "mongodb://localhost:27017/url-shortner"
+const DB_URI = "mongodb+srv://sagar:sagar@api-roxce.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
-const connectToDb = async () =>{
+const connectToDb = async () => {
     try {
-       const connect =  await mongoose.connect(DB_URI,connectionOptions)
-        if(connect) console.log(`Database connected successfully`.yellow.bold.underline)
+        const connect = await mongoose.connect(DB_URI, connectionOptions)
+        if (connect) console.log(`Database connected successfully`.yellow.bold.underline)
     } catch (error) {
         console.log(error)
     }
